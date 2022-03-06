44914f9cc33d00fbb1f6f19bba2aec0ca45bfe31
Nadia <kasimnadia@gmail.com>
2022-03-06 00:36:49 -0500

    add skeleton of submission message

--- src/User/UserRequest/Submission.js
+++ src/User/UserRequest/Submission.js
@@ -2,6 +2,23 @@ import React from 'react'
 
 export const Submission = () => {
   return (
-    <div>submission</div>
+    <div>
+      
+      submission
+
+      <h1>Your Task has been submitted</h1>
+
+      <div>Go to meeting now</div>
+
+      <h4>Would you like to be notified when a mentor is available to help you?</h4>
+      
+      <div>Enter your phone number to receive a SMS</div>
+
+
+    </div>
+
+    
   )
 }
+
+export default Submission;
